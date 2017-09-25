5952444b734ea951fcb34c3a11c5120b325b8e14
arodrigu46 <30849124+arodrigu46@users.noreply.github.com>
2017-09-25 10:56:36 -0500

    0.1

--- app/src/main/java/com/example/arodrigu46/tallerlistview/Principal.java
+++ app/src/main/java/com/example/arodrigu46/tallerlistview/Principal.java
@@ -1,13 +1,26 @@
 package com.example.arodrigu46.tallerlistview;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Principal extends AppCompatActivity {
-
+    private ListView lv;
+    private Resources resources;
+    private String opc[];
+    private Intent in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        lv = (ListView) findViewById(R.id.Opciones);
+        resources=this.getResources();
+        opc = resources.getStringArray(R.array.OpcionesPrincipal);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,opc);
+        lv.setOnItemClickListener(AdapterView);
     }
 }
