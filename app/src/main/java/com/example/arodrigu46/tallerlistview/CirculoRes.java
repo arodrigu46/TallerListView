30b6328308b459584c41ae95ae8d90c2f128bc95
arodrigu46 <30849124+arodrigu46@users.noreply.github.com>
2017-09-26 08:05:35 -0500

    interfaz 9/10

---
+++ app/src/main/java/com/example/arodrigu46/tallerlistview/CirculoRes.java
@@ -0,0 +1,13 @@
+package com.example.arodrigu46.tallerlistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CirculoRes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circulo_res);
+    }
+}
